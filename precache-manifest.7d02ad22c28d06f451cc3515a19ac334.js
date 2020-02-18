a7f79bb7354228faa5dcde94851e6b538cb3e47d
Tom Russell <TR115251@hotmail.co.uk>
2020-02-18 15:41:32 +0000

    Updates

---
+++ precache-manifest.7d02ad22c28d06f451cc3515a19ac334.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb253a5dfe8919f54ed1c21774eaffe",
+    "url": "/svgsnake/index.html"
+  },
+  {
+    "revision": "057bcea8b4bad5e82a0c",
+    "url": "/svgsnake/static/js/2.f0ae7a54.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/svgsnake/static/js/2.f0ae7a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfc1512a40e69f70945",
+    "url": "/svgsnake/static/js/main.e49534d1.chunk.js"
+  },
+  {
+    "revision": "6f500f84f6c84c7e8326",
+    "url": "/svgsnake/static/js/runtime-main.84fba756.js"
+  }
+]);
